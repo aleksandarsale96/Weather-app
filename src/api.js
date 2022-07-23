6ace3dec70f318a1dd9afe72ccdcc949d7f45a62
salelibertas <salelibertas@gmail.com>
2022-07-23 11:08:05 +0200

    weather-app

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": '52bb89bd27mshf53fec79146f447p1f0199jsn855930d8fb55',
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "15c9dae95b84dcc9a44316db66a6207f";
